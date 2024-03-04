44cb81bb354d985588a4f10f3e57c646b8295250
Lesalon_Steve... <lesalonsteven@gmail.com>
2024-03-04 09:51:37 +0100

    Saving functionality

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from tkinter import *
 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    site = web_field.get()
+    user = mail_field.get()
+    passk = pass_field.get()
+    user_data = f"{site}, {user}, {passk}"
+    f = open("data.txt", "a")
+    f.write(f'{user_data}\n')
+    f.close()
+    web_field.delete(0,"end")
+    mail_field.delete(0, "end")
+    pass_field.delete(0, "end")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 screen = Tk()
@@ -17,11 +30,13 @@ canvas.grid(row=0, column=1)
 website = Label(text="Website:")
 website.grid(row=1, column=0)
 web_field = Entry(width=35)
+web_field.focus()
 web_field.grid(row=1, column=1, columnspan=2)
 
 email = Label(text="Email/Username:")
 email.grid(row=2, column=0)
 mail_field = Entry(width=35)
+mail_field.insert(0, "youremail@whatever.com")
 mail_field.grid(row=2, column=1, columnspan=2)
 
 password = Label(text="Password:")
@@ -32,7 +47,7 @@ pass_field.grid(row=3, column=1)
 generate = Button(text="Generate Password")
 generate.grid(row=3, column=2)
 
-add = Button(text="add", width=33)
+add = Button(text="add", width=33, command=save)
 add.grid(row=4, column=1, columnspan=2)
 
 screen.mainloop()
